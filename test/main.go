a911a4fcb4a61db7fac9c898225ea9f091784939
Shotaro Murai <show.murai@gmail.com>
2014-07-19 23:22:18 +0900

    とりあえずコピペのPOST,GETを実装して動きをみる

--- test/main.go
+++ test/main.go
@@ -7,6 +7,13 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
+	r.GET("/someGet", getting)
+	r.POST("/somePost", posting)
+	r.PUT("/somePut", putting)
+	r.DELETE("/someDelete", deleting)
+	r.PATCH("/somePatch", patching)
+	r.HEAD("/someHead", head)
+	r.OPTIONS("/someOptions", options)
 
 	// Listen and server on 0.0.0.0:8080
 	r.Run(":9999")
