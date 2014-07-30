ce30cbfa44eab3a2552586c04ba9086d28ea932a
Shotaro Murai <shotaro.murai@soliton.co.jp>
2014-07-30 18:20:51 +0900

    Using Go-lang standard library sample.

---
+++ FilePhowa/temp/receive.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file") // the FormFile function takes in the POST input id file
+	defer file.Close()
+
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	out, err := os.Create("./uploadfile")
+	if err != nil {
+		fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
+		return
+	}
+
+	defer out.Close()
+
+	// write the content from POST to the file
+	_, err = io.Copy(out, file)
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
+
+	fmt.Fprintf(w, "File uploaded successfully : ")
+	fmt.Fprintf(w, header.Filename)
+}
+
+func main() {
+	http.HandleFunc("/", uploadHandler)
+	http.ListenAndServe(":9999", nil)
+}
